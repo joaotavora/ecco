ea00ceec6556614f4bb30cb2c662b09e8b0f03dc
João Távora <joaotavora@gmail.com>
2013-02-05 22:45:50 +0000

    bad attempt at using blobs of markdown and code, then splitting

--- ecco.el
+++ ecco.el
@@ -1,50 +1,18 @@
+;; **ecco** is a port of docco
+;;
+;; blablabla
+;;
+;;
 (require 'newcomment)
-(require 'htmlfontify)
+(require 'cl)
 
-(defun ecco ()
-  (interactive)
-  (let ((groups (ecco--gather-groups))
-        (title (buffer-name (current-buffer))))
-    (with-current-buffer
-        (get-buffer-create (format "*ecco for %s*" title))
-      (erase-buffer)
-      (insert (format "
-<!DOCTYPE html> 
-<html> 
-<head> 
-    <meta http-eqiv='content-type' content='text/html;charset=utf-8'> 
-    <title>%s</title> 
-    <link rel=stylesheet href=\"http://jashkenas.github.com/docco/resources/docco.css\"> 
-</head> 
-<body> 
-<div id=container> 
-    <div id=background></div> 
-    <table cellspacing=0 cellpadding=0> 
-    <thead> 
-      <tr> 
-        <th class=docs><h1>%s</h1></th> 
-        <th class=code></th> 
-      </tr> 
-    </thead> 
-    <tbody> " title title))
-      (dolist (group groups)
-        (insert "<tr><td class='docs'>")
-        (insert (ecco--render-comments (car group)))
-        (insert "</td><td class=code>")
-        (insert (ecco--render-code (cdr group)))
-        (insert "</td></tr>"))
-      (insert "</tbody> 
-    </table> 
-</div> 
-</body> 
-</html>")
-      (goto-char (point-min))
-      (pop-to-buffer (current-buffer)))))
-
-(defvar ecco-markdown-program "z:/holy/bin/common/markdown.pl")
-(defvar ecco-pygmentize-program "/usr/bin/pygmentize")
-(defvar ecco-use-pygments t)
-
+
+;;; Parsing
+;;; -------
+;;;
+;;; The idea is to gather pairs of comments and code snippets
+;;; and render them
+;;;
 (defun ecco--gather-groups ()
   "Returns a list of conses of strings (COMMENT . CODE)"
   (save-excursion
@@ -78,7 +46,47 @@
                     comments
                     code-snippets)))))
 
-(defun ecco--render-code (text)
+(defvar ecco--markdown-separator "<div class='ecco'>ecco-comment-separator<\div>")
+(defvar ecco--pygments-separator "\n--ecco-snippet-separator--\n")
+
+(defun ecco--render-groups (groups)
+  (let* ((blobs
+          (loop for ((comment . snippet)) on groups
+                collect comment into comments
+                collect snippet into snippets
+                finally return (list (mapconcat #'identity
+                                                comments
+                                                ecco--markdown-separator
+                                                )
+                                     (mapconcat #'identity
+                                                snippets
+                                                ecco--pygments-separator))))
+         (rendered-comment-blob (ecco--render-comments (first blobs)))
+         (rendered-snippet-blob (ecco--render-snippets (second blobs))))
+    (split-string )
+    (map 'list #'(lambda (a b) (cons a b))
+         (split-string rendered-comment-blob ecco--markdown-separator)
+         (split-string rendered-snippet-blob ecco--pygments-separator)
+         code-snippets)
+
+
+
+    ()
+    )
+
+  )
+
+
+;;; **ecco** renders:
+;;;
+;;; - comments through markdown
+;;;
+;;; - code through pygments or through emacs's built-int
+;;;   `htmlfontify`, in case pygments isn't available or the user
+;;;   set `ecco-use-pygments` to `nil`
+;;;
+;;;
+(defun ecco--render-snippets (text)
   "Return TEXT with span classes based on its fontification."
   (if ecco-use-pygments
       (ecco--pipe-text-through-program text (format "%s -g -f html"
@@ -95,6 +103,20 @@
   "Return markdown output for TEXT."
   (ecco--pipe-text-through-program text ecco-markdown-program))
 
+;;; For now, ecco needs the user to customize these to the paths he
+;;; needs using `setq` or similar
+;;;
+(defvar ecco-markdown-program "markdown")
+(defvar ecco-pygmentize-program "pygmentize")
+(defvar ecco-use-pygments t)
+
+
+
+;;; Piping to external processes
+;;; ----------------------------
+;;;
+;;; ecco uses `shell-command-on-region`
+
 (defun ecco--pipe-text-through-program (text program)
   (let ((temp-buffer (generate-new-buffer "*ecco-processing-temp*"))
         (max-mini-window-height 0))
@@ -103,6 +125,58 @@
       (shell-command-on-region (point-min) (point-max) program temp-buffer))
     (with-current-buffer temp-buffer
       (buffer-substring-no-properties (point-min) (point-max)))))
+
+
+;;; Main entry point
+;;; ----------------
+;;;
+
+(defun ecco ()
+  (interactive)
+  (let ((groups (ecco--gather-groups))
+        (title (buffer-name (current-buffer))))
+    (with-current-buffer
+        (get-buffer-create (format "*ecco for %s*" title))
+      (erase-buffer)
+      (insert (format "
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-eqiv='content-type' content='text/html;charset=utf-8'>
+    <title>%s</title>
+    <link rel=stylesheet href=\"http://jashkenas.github.com/docco/resources/docco.css\">
+</head>
+<body>
+<div id=container>
+    <div id=background></div>
+    <table cellspacing=0 cellpadding=0>
+    <thead>
+      <tr>
+        <th class=docs><h1>%s</h1></th>
+        <th class=code></th>
+      </tr>
+    </thead>
+    <tbody> " title title))
+      (dolist (group groups)
+        (insert "<tr><td class='docs'>")
+        (insert (ecco--render-comments (car group)))
+        (insert "</td><td class=code>")
+        (insert (ecco--render-code (cdr group)))
+        (insert "</td></tr>"))
+      (insert "</tbody>
+    </table>
+</div>
+</body>
+</html>")
+      (goto-char (point-min))
+      (pop-to-buffer (current-buffer)))))
+
+
+
+;;; Debug functions
+;;; ---------------
+;;;
+;;; for now, the only debug function is `ecco--gather-groups-debug`
 
 (defun ecco--gather-groups-debug ()
   (interactive)
@@ -115,5 +189,16 @@
         (insert (car group))
         (insert "-**- SNIPPET -**-\n")
         (insert (cdr group)))
+      (goto-char (point-min))
+      (pop-to-buffer (current-buffer)))))
+
+(defun ecco--render-groups-debug ()
+  (interactive)
+  (let ((blobs (ecco--render-groups (ecco--gather-groups))))
+    (with-current-buffer
+        (get-buffer-create (format "*ecco--debug for %s*" (buffer-name (current-buffer))))
+      (erase-buffer)
+      (insert (first blobs))
+      (insert (second blobs))
       (goto-char (point-min))
       (pop-to-buffer (current-buffer)))))
